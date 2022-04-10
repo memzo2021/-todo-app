5b01d15e9e48b7367f152741a68dbf6bec45562f
MahomedM <mahomedmachel@gmail.com>
2022-04-10 15:16:56 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todoapp-angular';
+  title = '# todo';
+  list: any[] = [];
+  ajoutTache(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+    console.log(this.list);
+  }
+  supprimerTache(id: number) {
+    console.log(id)
+    this.list = this.list.filter(item => item.id !== id);
+  }
 }
+/**
+ * 
+ * 
+ */
